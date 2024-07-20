953c6b4782cfa54ca232803665a05e01ad5fb35d
AntolKun <aloysiusluilui@gmail.com>
2024-07-20 22:22:13 +0700

    cihuyy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.UserTemplate')
 @section('content')
 
+<div class="row">
+  <div class="col-lg-6"><img src="{{ asset('/guru_fotos/pakcucus.jpeg') }}" alt=""></div>
+  <div class="col-lg-6"><img src="{{ asset('/guru_fotos/cihuy.webp') }}" alt=""></div>
+  <div class="col-lg-6"></div>
+</div>
+
 @endsection
